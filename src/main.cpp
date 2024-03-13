be449da5db2d2251a1df44c5a2d062d33ef57b00
marcel-gerber <marcelgerber9@gmail.com>
2024-03-13 01:57:44 +0100

    Fixed bug when initializing SQUARES_BETWEEN

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,25 @@
 #include <iostream>
-#include "color.hpp"
 #include "board.hpp"
 #include "fen_parser.hpp"
 #include "attacks.hpp"
-#include "misc/find_magics.hpp"
+#include "movegen.hpp"
 
 int main() {
+    Attacks::initMagics();
+    MoveGen::initSquaresBetween();
+
     Board board = Board();
-    board.getCastlingRights()->set(Castling::BLACK_000);
-    board.getCastlingRights()->set(Castling::WHITE_00);
-    board.getCastlingRights()->unset(Castling::WHITE_00);
 
-    std::cout << board.getCastlingRights()->has(Castling::WHITE_00) << std::endl;
-    std::cout << +board.getCastlingRights()->getCastlingRights() << std::endl;
-
-    auto square = Square(Square::Value::E7);
-    std::cout << +square.getIndex();
-
-    Fen::setFen(board, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-    std::cout << +board.getHalfMoveClock() << std::endl;
-    std::cout << +board.getSideToMove() << std::endl;
-    std::cout << +board.getCastlingRights()->getCastlingRights() << std::endl;
-    std::cout << +board.getEnPassantSquare()->getIndex() << std::endl;
+//    Fen::setFen(board, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+//    std::cout << +board.getHalfMoveClock() << std::endl;
+//    std::cout << +board.getSideToMove() << std::endl;
+//    std::cout << +board.getCastlingRights()->getCastlingRights() << std::endl;
+//    std::cout << +board.getEnPassantSquare()->getIndex() << std::endl;
 
     Fen::setFen(board, "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 10");
     board.print();
 
-    Attacks::initMagics();
+    auto square = Square(Square::Value::E7);
     uint64_t attacks = Attacks::getQueenAttacks(square, board.getOccupancy());
     Misc::printBits(attacks);
 
--- src/movegen.hpp
+++ src/movegen.hpp
@@ -10,22 +10,24 @@
 #include "attacks.hpp"
 
 class MoveGen {
-public:
+private:
     static inline uint64_t SQUARES_BETWEEN[64][64] = { 0 };
 
 public:
     static void initSquaresBetween() {
-        uint64_t squares = 0ULL;
+        uint64_t squares_bb = 0ULL;
         uint64_t attacks = 0ULL;
 
         for(Square square1 = 0; square1 < 64; square1++) {
             for(Square square2 = 0; square2 < 64; square2++) {
-                squares = (1 << square1.getIndex()) | (1 << square2.getIndex());
+                if(square1.getIndex() == square2.getIndex()) continue;
+
+                squares_bb = (1ULL << square1.getIndex()) | (1ULL << square2.getIndex());
 
                 if(square1.getFileIndex() == square2.getFileIndex()
                 || square1.getRankIndex() == square2.getRankIndex()) {
 
-                    attacks = Attacks::getRookAttacks(square1, squares) & Attacks::getRookAttacks(square2, squares);
+                    attacks = Attacks::getRookAttacks(square1, squares_bb) & Attacks::getRookAttacks(square2, squares_bb);
                     SQUARES_BETWEEN[square1.getIndex()][square2.getIndex()] = attacks;
 
                     continue;
@@ -34,7 +36,7 @@ public:
                 if(square1.getDiagonalIndex() == square2.getDiagonalIndex()
                 || square1.getAntiDiagonalIndex() == square2.getAntiDiagonalIndex()) {
 
-                    attacks = Attacks::getBishopAttacks(square1, squares) & Attacks::getBishopAttacks(square2, squares);
+                    attacks = Attacks::getBishopAttacks(square1, squares_bb) & Attacks::getBishopAttacks(square2, squares_bb);
                     SQUARES_BETWEEN[square1.getIndex()][square2.getIndex()] = attacks;
 
                     continue;
