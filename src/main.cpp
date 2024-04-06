62e1a7c684b1d72218071eedf8fcd6c65c967165
Marcel Gerber <marcelgerber9@gmail.com>
2024-04-07 00:22:31 +0200

    Found fen where perft at depth 3 is not correct

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,17 @@
 #include "attacks.hpp"
 #include "movegen.hpp"
 #include "perft.hpp"
+#include "debug.hpp"
 
 int main() {
     Attacks::initMagics();
     MoveGen::initSquaresBetween();
 
+//    Debug::run("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
+
     Board board = Board();
 
-    Fen::setFen(board, "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
+//    Fen::setFen(board, "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
 //    std::cout << +board.getHalfMoveClock() << std::endl;
 //    std::cout << +board.getSideToMove() << std::endl;
 //    std::cout << +board.getCastlingRights()->getCastlingRights() << std::endl;
@@ -29,10 +32,11 @@ int main() {
 
     // Fen::setFen(board, "r1bqk1nr/p1pp1ppp/1p1np3/6b1/1Q6/5N2/PPP4P/R3K2R w KQkq - 0 1");
 
-//    Fen::setFen(board, "k7/8/8/3pP1K1/8/8/8/8 w - d6 0 1");
+    // TODO: this FEN delivers an incorrect number of nodes at depth 3
+    Fen::setFen(board, "2r1kr2/p4p1n/2N3pb/6P1/3pq3/1B5p/PP2KP1P/R1B3NR w - - 0 21");
 
     Perft perft(board);
-    perft.run(4);
+    perft.run(3);
 
 //     board.print();
 
