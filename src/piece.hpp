de5623473d491073fb34cbeef63e8f4663ce4817
Marcel Gerber <marcelgerber9@gmail.com>
2024-06-04 23:04:39 +0200

    Fixed bug in constructor and getType

--- src/piece.hpp
+++ src/piece.hpp
@@ -81,8 +81,16 @@ public:
     }
 
     Piece(PieceType type, Color color) {
-        if(color == Color::NONE) piece = Piece::NONE;
-        if(type.getValue() == PieceType::NONE) piece = Piece::NONE;
+        if(color == Color::NONE) {
+            piece = Piece::NONE;
+            return;
+        }
+
+        if(type.getValue() == PieceType::NONE) {
+            piece = Piece::NONE;
+            return;
+        }
+
         piece = static_cast<Value>(color.getValue() * 6 + type.getIndex());
     }
 
@@ -179,6 +187,7 @@ public:
     }
 
     [[nodiscard]] PieceType getType() const {
+        if(piece == NONE) return PieceType::NONE;
         return static_cast<PieceType::Value>((uint8_t) piece % 6);
     }
 
