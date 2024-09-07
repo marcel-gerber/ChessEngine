f7f00fc30947543a6af2e58a3e3059203c5a371e
Marcel Gerber <marcelgerber9@gmail.com>
2024-09-08 01:15:18 +0200

    Implemented ucinewgame, position and go commands

--- src/uci.hpp
+++ src/uci.hpp
@@ -5,6 +5,11 @@
 #ifndef CHESSENGINE_UCI_HPP
 #define CHESSENGINE_UCI_HPP
 
+#include "board.hpp"
+#include "constants.hpp"
+#include "grid.hpp"
+#include "search.hpp"
+
 #include <iostream>
 #include <memory>
 #include <sstream>
@@ -36,10 +41,105 @@ public:
     }
 };
 
+class UCICommandUCINewGame : public UCICommand {
+
+private:
+    Board &board;
+
+public:
+    UCICommandUCINewGame(Board &b) : board(b) { }
+
+    void execute(const std::vector<std::string> &args) override {
+        board = Board();
+    }
+};
+
+class UCICommandPosition : public UCICommand {
+
+private:
+    Board &board;
+
+    Move uciToMove(const std::string &input) {
+        const std::string s_from = input.substr(0, 2);
+        const std::string s_to = input.substr(2, 2);
+
+        const Square from = Square(s_from);
+        const Square to = Square(s_to);
+        const auto piece = board.getPiece(to.getIndex());
+
+        // En Passant Move
+        if(piece.getType().getValue() == PieceType::PAWN && to.getIndex() == board.getEnPassantSquare()->getIndex()) {
+            return Move::create<MoveType::EN_PASSANT>(from.getIndex(), to.getIndex());
+        }
+
+        // Castling Move
+        if(piece.getType().getValue() == PieceType::KING && std::abs(from.getIndex() - to.getIndex()) == 2) {
+            return Move::create<MoveType::CASTLING>(from.getIndex(), to.getIndex());
+        }
+
+        switch(input.size()) {
+            case 4:
+                return Move::create<MoveType::NORMAL>(from.getIndex(), to.getIndex());
+            case 5:
+                const auto promotion_piece = Piece(input[4]);
+                return Move::create<MoveType::PROMOTION>(from.getIndex(), to.getIndex(), promotion_piece.getType());
+        }
+        return { };
+    }
+
+public:
+    UCICommandPosition(Board &b) : board(b) { }
+
+    void execute(const std::vector<std::string> &args) override {
+        if(args.empty()) return;
+        int index = 0;
+
+        if(args[0] == "startpos") {
+            index = 1;
+            board.setFen(Constants::START_POS);
+        } else if(args[0] == "fen" && !args[1].empty()) {
+            // TODO: This is not correct as a FEN string can be longer than 1 argument
+            index = 2;
+            board.setFen(args[1]);
+        }
+
+        if(args[index] == "moves") {
+            index++;
+
+            while(index < args.size()) {
+                const std::string &uci_move = args[index];
+                const auto move = uciToMove(uci_move);
+
+                board.makeMove(move);
+                index++;
+            }
+            board.print();
+        }
+    }
+};
+
+class UCICommandGo : public UCICommand {
+
+private:
+    Board &board;
+    Search search;
+
+public:
+    UCICommandGo(Board &b) : board(b), search(b) { }
+
+    void execute(const std::vector<std::string> &args) override {
+        if(args.empty()) {
+            search.search(Constants::STANDARD_DEPTH);
+            std::cout << "bestmove " << search.getBestMove().toUCI() << std::endl;
+        }
+    }
+};
+
 class UCIHandler {
 
 private:
     std::unordered_map<std::string, std::unique_ptr<UCICommand>> commands;
+    Board board;
 
     static std::pair<std::string, std::vector<std::string>> split(const std::string &input) {
         std::vector<std::string> arguments;
@@ -58,8 +158,13 @@ private:
 
 public:
     UCIHandler() {
+        board = Board();
+
         commands["uci"] = std::make_unique<UCICommandUCI>();
         commands["isready"] = std::make_unique<UCICommandIsReady>();
+        commands["ucinewgame"] = std::make_unique<UCICommandUCINewGame>(board);
+        commands["position"] = std::make_unique<UCICommandPosition>(board);
+        commands["go"] = std::make_unique<UCICommandGo>(board);
     }
 
     void handleCommand(const std::string &input) {
