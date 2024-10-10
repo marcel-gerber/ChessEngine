b6fef13d8f288c1662931968caf8be25b7bce230
Marcel Gerber <marcelgerber9@gmail.com>
2024-10-10 19:58:19 +0200

    Documented perft.hpp

--- src/perft.hpp
+++ src/perft.hpp
@@ -1,7 +1,3 @@
-//
-// Created by Marcel on 22.03.2024.
-//
-
 #ifndef CHESSENGINE_PERFT_HPP
 #define CHESSENGINE_PERFT_HPP
 
@@ -14,13 +10,16 @@ class Perft {
 private:
     Board &board;
 
+    /// Returns number of all legal moves given the depth
     [[nodiscard]] uint64_t perft(int depth) const;
 
 public:
     explicit Perft(Board &board);
 
+    /// Runs perft test. Prints information when done
     void run(int depth);
 
+    /// Returns number of all legal moves given the depth
     [[nodiscard]] uint64_t nodes(int depth) const;
 };
 
